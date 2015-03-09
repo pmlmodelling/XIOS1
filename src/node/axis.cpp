0d93f89136bbb6035fb89d80e72aa9f46faba108
rlacroix <rlacroix@d613d254-d18a-4304-b296-5125d4c4d670>
2015-03-09 09:04:51 +0000

    Improve the error messages when checking the axis attributes.
    git-svn-id: http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/branchs/xios-1.0@566 d613d254-d18a-4304-b296-5125d4c4d670

--- src/node/axis.cpp
+++ src/node/axis.cpp
@@ -8,7 +8,7 @@
 #include "xmlioserver_spl.hpp"
 
 namespace xios {
-   
+
    /// ////////////////////// Définitions ////////////////////// ///
 
    CAxis::CAxis(void)
@@ -53,29 +53,31 @@ namespace xios {
    {
       if (this->isChecked) return;
       if (this->size.isEmpty())
-         ERROR("CAxis::checkAttributes(void)",<< "Attribut <size> of the axis must be specified") ;
+         ERROR("CAxis::checkAttributes(void)",
+               << "Attribute <size> of the axis [ id = '" << getId() << "' , context = '" << CObjectFactory::GetCurrentContextId() << "' ] must be specified");
       StdSize size = this->size.getValue();
-      
-      StdSize zoom_begin,zoom_end, zoom_size ;
-      
-      zoom_begin = (this->zoom_begin.isEmpty()) ?  1 : this->zoom_begin.getValue() ;
-      zoom_end = (this->zoom_end.isEmpty()) ?  size : this->zoom_end.getValue() ; 
-      zoom_size = (this->zoom_size.isEmpty()) ?  size : this->zoom_size.getValue() ;
-      
-      if (this->zoom_begin.isEmpty()) zoom_begin=zoom_end-zoom_size+1 ;
-      if (this->zoom_end.isEmpty()) zoom_end=zoom_begin+zoom_size-1 ;
-      if (this->zoom_size.isEmpty()) zoom_size=zoom_end-zoom_begin+1 ;
-      
+
+      StdSize zoom_begin,zoom_end, zoom_size;
+
+      zoom_begin = (this->zoom_begin.isEmpty()) ?  1 : this->zoom_begin.getValue();
+      zoom_end = (this->zoom_end.isEmpty()) ?  size : this->zoom_end.getValue();
+      zoom_size = (this->zoom_size.isEmpty()) ?  size : this->zoom_size.getValue();
+
+      if (this->zoom_begin.isEmpty()) zoom_begin=zoom_end-zoom_size+1;
+      if (this->zoom_end.isEmpty()) zoom_end=zoom_begin+zoom_size-1;
+      if (this->zoom_size.isEmpty()) zoom_size=zoom_end-zoom_begin+1;
+
       if ( (zoom_begin < 1) || (zoom_begin > size) || (zoom_end<1) || (zoom_end>size) || (zoom_size<1) || (zoom_size>size) || (zoom_begin>zoom_end))
-        ERROR("CAxis::checkAttributes(void)",<< "One or more attribut of <zoom_begin>, <zoom_end>, <zoom_size>, are not well specified") ;
-      this->zoom_begin.setValue(zoom_begin) ;
-      this->zoom_end.setValue(zoom_end) ;
-      this->zoom_size.setValue(zoom_size) ;
-      
+        ERROR("CAxis::checkAttributes(void)",
+              << "One or more attributes among <zoom_begin>, <zoom_end>, <zoom_size> of axis [ id = '" << getId() << "' , context = '" << CObjectFactory::GetCurrentContextId() << "' ] are not well specified");
+      this->zoom_begin.setValue(zoom_begin);
+      this->zoom_end.setValue(zoom_end);
+      this->zoom_size.setValue(zoom_size);
+
       StdSize true_size = value.numElements();
       if (size != true_size)
          ERROR("CAxis::checkAttributes(void)",
-               << "The array \'value\' has a different size that the one defined by the \'size\' attribut")
+               << "The array \'value\' of axis [ id = '" << getId() << "' , context = '" << CObjectFactory::GetCurrentContextId() << "' ] has a different size that the one defined by the \'size\' attribute");
 
       this->isChecked = true;
    }
